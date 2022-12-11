fbd72a707d2ff91e934409f2e5939def5c837ef6
Patricio Urrutia <purrutia@gmail.com>
2022-12-11 00:12:44 -0300

    Start AoC01 in python

---
+++ 2022/01/2022-aoc-01.py
@@ -0,0 +1,8 @@
+# AOC 2022-01
+
+with open('input.test','r') as inputfile:
+    # Read each  lineof the file and store it as a list
+    lines = inputfile.readlines()
+
+    for line in lines:
+        print(line)
